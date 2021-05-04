edb6512843bbff117cd5dc7ceab8b653b8918b0e
Hugo <hugolacerdar@gmail.com>
2021-05-04 13:22:10 -0300

    Fixing: negative numbers support

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -5,7 +5,7 @@ defmodule ListFilter do
 
   defp filter([_head | _tail] = list) do
     list
-    |> Stream.filter(fn elem -> String.match?(elem, ~r/^[0-9]*$/) end)
+    |> Stream.filter(fn elem -> String.match?(elem, ~r/^-[0-9]|^[0-9]*$/) end)
     |> Stream.map(fn elem -> String.to_integer(elem) end)
     |> Enum.count(fn elem -> Integer.is_odd(elem) end)
   end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -3,7 +3,7 @@ defmodule ListFilterTest do
 
   describe "call/1" do
     test "given a list of strings, should filter numeric values and return the quantity of odd numbers" do
-      list = ["1", "3", "6", "43", "banana", "6", "abc"]
+      list = ["1", "3", "6", "-43", "banana", "6", "abc"]
       expected = 3
       response = ListFilter.call(list)
 
